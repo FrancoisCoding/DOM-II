6aafcedeae6809f79387ff9cdf69c2e916aa0444
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-15 15:38:25 -0400

    Added MouseOver/Click to Destination Boat Img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+// Sounds
+const waves = document.getElementById("waves");
+const clickMe = document.getElementById("clickMe");
+const boat = document.getElementById("boat");
+var soundFlag = true;
+
+// On click
+boat.onclick = function () {
+    if (soundFlag) {
+        waves.pause();
+        waves.currentTime = 0;
+        waves.play();
+    }
+}
+
+// Mouse Over
+boat.onmouseover = function () {
+    if (soundFlag) {
+        clickMe.pause();
+        clickMe.currentTime = 0;
+        clickMe.play();
+    }
+}
